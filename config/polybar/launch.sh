b8c293ac42819b38f4965f43b617649572e5bc28
Samuel Adam Suhendra <samuel.suhendra@gmail.com>
2018-01-09 22:05:01 +0700

    Update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,14 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+for monitor in $(xrandr --listmonitors | grep -v Monitor | cut -d ' ' -f 6); do
 # Launch bar1 and bar2
-DISPLAY1="$(xrandr -q | grep 'LVDS2' | cut -d ' ' -f1)"
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar top &
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar bottom &
-
-DISPLAY2="$(xrandr -q | grep 'VGA2' | cut -d ' ' -f1)"
-[[ ! -z "$DISPLAY2" ]] && MONITOR=$DISPLAY2 polybar top &
-[[ ! -z "$DISPLAY2" ]] && MONITOR=$DISPLAY2 polybar bottom &
+    [[ ! -z "$monitor" ]] && MONITOR="$monitor" polybar top &
+    [[ ! -z "$monitor" ]] && MONITOR="$monitor" polybar bottom &
+done
 
 # Launch bar1 and bar2
 polybar top &
